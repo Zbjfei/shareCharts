55f98c50a47d09f262e7ab9495ccfc23cf880cce
zbjfei <zbjfei@qq.com>
2024-10-19 22:05:52 +0300

    add Actions

---
+++ db/DBStruct.js
@@ -0,0 +1,12 @@
+// ----------------------------
+// Collection structure for s_ShareUsers
+// ----------------------------
+db.getCollection("s_ShareUsers").drop();
+db.createCollection("s_ShareUsers");
+
+// ----------------------------
+// Collection structure for s_ShareCharts
+// ----------------------------
+db.getCollection("s_ShareCharts").drop();
+db.createCollection("s_ShareCharts");
+
